70fc3849b936eb03d652b8b8e12c74deb5cb2b93
sade-online <fot.fasan@gmail.com>
2022-01-20 21:18:47 +0000

    conversation function called and displayed in console

--- session-3/index.js
+++ session-3/index.js
@@ -1,12 +1,22 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-const myName = "Sade";
-const myAge = "20";
+// const myName = "Sade";
+// const myAge = "20";
 
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-console.log (a + b); // expected output = 15
+// console.log (a + b); // expected output = 15 
 
+function sayHey() {
+    console.log("Hey!");
+}
 
+function conversation() {
+    sayHey();
+    console.log ("How are you?");
+    console.log ("Goodbye");
+}
+
+// call conversation(); in console ... it works.. happy days :))
