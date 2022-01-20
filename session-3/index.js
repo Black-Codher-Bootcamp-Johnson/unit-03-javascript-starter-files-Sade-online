181882f2c7e42add925910bc8cbb4adbc54b7c00
sade-online <fot.fasan@gmail.com>
2022-01-20 22:05:47 +0000

    JS session 3 variables and function
    Task 1, 2, 3 completed

--- session-3/index.js
+++ session-3/index.js
@@ -1,13 +1,14 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-// const myName = "Sade";
-// const myAge = "20";
+const myName = "Sade";
+const myAge = "20";
+let a = 5;
+let b = 10;
 
-// let a = 5;
-// let b = 10;
+console.log (a + b); // expected output = 15 
 
-// console.log (a + b); // expected output = 15 
+// break 
 
 function sayHey() {
     console.log("Hey!");
@@ -18,5 +19,16 @@ function conversation() {
     console.log ("How are you?");
     console.log ("Goodbye");
 }
-
 // call conversation(); in console ... it works.. happy days :))
+
+// break 
+
+// below is a function called futureAge that returns a person’s name and their age in 5 years time
+
+function futureAge(userName, currentAge){
+
+    let newAge = currentAge + 5;
+    console.log("Hi " + userName + "! You will be " + newAge + " years old in 5 years time :)");
+
+    // to call in console using example variables = futureAge("Mary", 17);
+}
