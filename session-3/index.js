b208e06b88430a8f6025ecbcc1b54b98743ec34d
sade-online <fot.fasan@gmail.com>
2022-01-20 20:56:12 +0000

    changed var to let keyword

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,12 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+const myName = "Sade";
+const myAge = "20";
+
+let a = 5;
+let b = 10;
+
+console.log (a + b); // expected output = 15
+
+
