6d3bc06bb7348409474ef17043f865c11f5fe77d
sade-online <fot.fasan@gmail.com>
2022-01-31 21:55:44 +0000

    for loops and while loops

--- session-6/index.js
+++ session-6/index.js
@@ -3,41 +3,42 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
-let count = 0; // let browser know that count is 0. the start
-while (count < 10) {
+// let count = 0; // let browser know that count is 0.
+// while (count < 10) {
 
-    const randomNumber = Math.round(Math.random() * 10);
+//     const randomNumber = Math.round(Math.random() * 10);
 
-    count = count + 1;
-    console.log("This is the random number " + randomNumber)
+//     count++; // (or count = count + 1)
+//     console.log("This is the random number " + randomNumber)
 
-    console.log(count);
+//     console.log(count); // for testing
 
+// }
+
+
+
+// ****************************************************TASK 2; for loops ****************************************************
+
+
+for (let count = 0; // initialisation variable inside brackets
+    count < 10; // test condition for loop 
+    count++) { // to change value of counter
+        const randomNumber = Math.round(Math.random() * 10);
+
+        console.log("This is the random number " + randomNumber)
+    }
+
+
+
+// ****************************************************Practise ****************************************************
+// a while loop that prints the 2x tables until result is 144
+
+let multiplier = 1;
+
+while (multiplier <= 72) {
+    console.log (multiplier * 2)
+    multiplier++
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ****************************************************TASK 2****************************************************
-
-
-
-
-
 
 
 
